757a2e9e148a1b1e8d4330a385507f6baac480a7
Muhammad Dzurrafiq H <dzurrafiq7@gmail.com>
2024-04-02 13:01:21 +0700

    add file and first display

---
+++ app.js
@@ -0,0 +1,32 @@
+const p1Button = document.querySelector('#p1-button');
+const p2Button = document.querySelector('#p2-button');
+
+const p1Display = document.querySelector('#p1-display');
+const p2Display = document.querySelector('#p2-display');
+
+let p1Score = 0;
+let p2Score = 0;
+
+let winPoint = 10;
+let isGameOver = false;
+
+p1Button.addEventListener('click', () => {
+    if(!isGameOver){
+        p1Score += 1;
+        if (p1Score === winPoint) {
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+
+p2Button.addEventListener('click', () => {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winPoint) {
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+})
+
